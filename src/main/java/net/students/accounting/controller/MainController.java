d8bf272084653fca70d1f8ac05f28d548b0dd4ce
mazurkate8 <mazurkate8@gmail.com>
2021-11-16 13:30:18 +0200

    Add logging

--- src/main/java/net/students/accounting/controller/MainController.java
+++ src/main/java/net/students/accounting/controller/MainController.java
@@ -2,20 +2,26 @@ package net.students.accounting.controller;
 
 import net.students.accounting.entity.Student;
 import net.students.accounting.service.StudentService;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.MessageFormat;
 import java.util.List;
 
 @RestController
 @RequestMapping("/api")
 public class MainController {
 
+    private static final Logger LOGGER =  LogManager.getLogger(MainController.class);
+
     @Autowired
     StudentService studentService;
 
     @GetMapping("/students")
     public List<Student> getStudent() {
+        LOGGER.info(MessageFormat.format("Getting student from a DB{0}", studentService.getAllStudents()));
         return studentService.getAllStudents();
     }
 
